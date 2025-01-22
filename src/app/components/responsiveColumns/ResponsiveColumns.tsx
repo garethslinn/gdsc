f8d9a06c1f4add432c8ddcaee880a5973349b592
garethslinn <gslinn@gmail.com>
2025-01-22 20:00:25 +0000

    apply dynatic path

--- src/app/components/responsiveColumns/ResponsiveColumns.tsx
+++ src/app/components/responsiveColumns/ResponsiveColumns.tsx
@@ -1,14 +1,15 @@
-// components/ResponsiveColumns.tsx
-import React from 'react';
-import Image from 'next/image';
+"use client";
+
+import React from "react";
+import Image from "next/image";
 import {
     Container,
     Column,
     Logo,
     Paragraph,
-    LearnMoreLink
-} from './ResponsiveColumns.styles';
-import {useTheme} from "@/app/context/ThemeContext";
+    LearnMoreLink,
+} from "./ResponsiveColumns.styles";
+import { useTheme } from "@/app/context/ThemeContext";
 
 interface ColumnData {
     logo: string;
@@ -22,26 +23,39 @@ interface ResponsiveColumnsProps {
     columns: ColumnData[];
 }
 
-
 const ResponsiveColumns: React.FC<ResponsiveColumnsProps> = ({ columns }) => {
-    const { currentTheme: { theme } } = useTheme();
+    const {
+        currentTheme: { theme },
+    } = useTheme();
+
+    const basePath = process.env.NODE_ENV === "production" ? "/gdsc" : "";
 
     return (
         <Container>
-            {columns && columns.map((column, index) => (
-                <Column key={index}>
-                    <Logo color={column.color}>
-                        <Image
-                            src={theme === 'light' ? column.logoWhite : column.logoWhite}
-                            alt={`Logo for ${column.description}`}
-                            layout="fill"
-                            objectFit="contain"
-                        />
-                    </Logo>
-                    <Paragraph>{column.description}</Paragraph>
-                    <LearnMoreLink href={column.link} aria-label={`Learn more about ${column.description}`}>Learn more</LearnMoreLink>
-                </Column>
-            ))}
+            {columns &&
+                columns.map((column, index) => (
+                    <Column key={index}>
+                        <Logo color={column.color}>
+                            <Image
+                                src={`${basePath}${
+                                    theme === "light"
+                                        ? column.logoWhite
+                                        : column.logo
+                                }`}
+                                alt={`Logo for ${column.description}`}
+                                layout="fill"
+                                objectFit="contain"
+                            />
+                        </Logo>
+                        <Paragraph>{column.description}</Paragraph>
+                        <LearnMoreLink
+                            href={column.link}
+                            aria-label={`Learn more about ${column.description}`}
+                        >
+                            Learn more
+                        </LearnMoreLink>
+                    </Column>
+                ))}
         </Container>
     );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,10 @@ import Title from "@/app/components/title/Title";
 import Paragraph from "@/app/components/paragraph/Paragraph";
 import Card from "@/app/components/card/Card";
 import ResponsiveColumns from "@/app/components/responsiveColumns/ResponsiveColumns";
-import {intro} from "@/app/constants/casestudies/intro";
-// import {steps} from "@/app/constants/steps";
-// import StepIndicator from "@/app/components/stepIndicator/StepIndicator";
-
+import { intro } from "@/app/constants/casestudies/intro";
 
 const About: React.FC = () => {
+    const basePath = process.env.NODE_ENV === "production" ? "/gdsc" : "";
 
     return (
         <>
@@ -19,34 +17,32 @@ const About: React.FC = () => {
                 GDS Consulting offers elegant, custom-built solutions from start to finish, addressing the significant challenges you face. I build entire products while also tackling your immediate development and optimisation needs.
             </Paragraph>
 
-            {/*<StepIndicator steps={steps} currentStep={1} />*/}
-
             <ResponsiveColumns columns={intro} />
             <Card
                 title="Frontend Development"
                 link="/development"
-                imageSrc="/assets/images/frontend.svg"
+                imageSrc={`${basePath}/assets/images/frontend.svg`}
             >
                 I excel in building products primarily around frontend technologies. The world of frontend moves fast, but my extensive experience with developing open source, rapid performance, and advanced UX/UI ensures that I can contribute effectively and stay updated with new tech.
             </Card>
             <Card
                 title="Backend Development"
                 link="/development"
-                imageSrc="/assets/images/backend.svg"
+                imageSrc={`${basePath}/assets/images/backend.svg`}
             >
                 My backend stack includes TypeScript, Express, NodeJS, and MongoDB, ensuring your backend is robust and efficient. I can also assist with Python as when needed.
             </Card>
             <Card
                 title="Performance Optimisation"
                 link="/case-studies"
-                imageSrc="/assets/images/optimisation.svg"
+                imageSrc={`${basePath}/assets/images/optimisation.svg`}
             >
                 Scalability problems can be a good sign of growth, but they can also be painful. I analyse potential bottlenecks in your code and infrastructure, propose a prioritised backlog of improvements, and bring them to life. I will prepare your product for user surges, making it reliable, resource-efficient, and scalable.
             </Card>
             <Card
                 title="Product Design"
                 link="/graphic-design"
-                imageSrc="/assets/images/design.svg"
+                imageSrc={`${basePath}/assets/images/design.svg`}
             >
                 Whether you have just the hint of an idea or are further along in product development, I dive in to produce interfaces where beauty and functionality combine to provide users with an exceptional experience.
             </Card>
